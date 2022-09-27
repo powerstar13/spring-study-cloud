7339dc242720ee1c0c0fa30a9c87c2786e9b9c88
홍준성 <ghdwnstjd13@gmail.com>
2022-09-27 14:14:07 +0900

    DNS 구성 및 활용

---
+++ dnsapp/src/main/java/spring/study/dnsapp/DnsappApplication.java
@@ -0,0 +1,13 @@
+package spring.study.dnsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsappApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DnsappApplication.class, args);
+    }
+    
+}
